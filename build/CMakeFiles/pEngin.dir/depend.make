09e6bad2c16426d26efde778c19c6255a9d1c09c
Kelson Wysocki <_your-GitHub-username_@users.noreply.github.com>
2021-07-28 11:21:29 -0700

    Removing unneeded files.

--- build/CMakeFiles/pEngin.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pEngin.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Kelson/Documents/ParticleEngine
-# Build directory: C:/Users/Kelson/Documents/ParticleEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
